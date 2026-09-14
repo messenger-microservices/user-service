1d3db789c32ec8da2430484f0b7339b6564bca4e
pulsarmn <152626412+pulsarmn@users.noreply.github.com>
2026-09-14 10:33:38 +0300

    feat: add liquibase migrations for User entity
    * chore: add Spring Boot plugin
    
    * chore: add neccessary dependencies for Liquibase migrations
    
    * chore: add compose.dev.yaml with development database service
    
    * feat: add Liquibase migration for user entity
    
    * feat: add application*.yaml configuration files for each profile
    
    * feat: add the UserApplication class-launcher
    
    * chore: add Spring Boot Starter Data JPA dependency
    
    * feat: add domain User entity

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,20 @@
 plugins {
     java
+    alias(libs.plugins.spring.boot)
 }
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation(platform(libs.spring.boot.dependencies))
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.liquibase)
+}
+
+dependencies {
+    runtimeOnly(libs.postgresql.driver)
 }
 
 configure<JavaPluginExtension> {
--- src/main/java/ru/pulsarmn/messenger/user/UserApplication.java
+++ src/main/java/ru/pulsarmn/messenger/user/UserApplication.java
@@ -1,9 +1,13 @@
 package ru.pulsarmn.messenger.user;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class UserApplication {
 
     static void main(String[] args) {
-
+        SpringApplication.run(UserApplication.class, args);
     }
 }
