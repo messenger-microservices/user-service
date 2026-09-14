02e66b685a0ad2ab70f23c3f70fe59f623045ae0
pulsar <pulsarmn@yandex.ru>
2026-09-14 11:15:04 +0300

    feat: add the UserRepository with `searchUsers()` method

---
+++ src/main/java/ru/pulsarmn/messenger/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package ru.pulsarmn.messenger.user.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.pulsarmn.messenger.user.domain.User;
+
+import java.util.UUID;
+
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query("SELECT u FROM User u WHERE u.username ILIKE CONCAT('%', :username, '%')")
+    Page<User> searchUsers(@Param("username") String username, Pageable pageable);
+}
