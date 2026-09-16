6bd8f0a9ae0921131162337ef696c6d680213bd5
pulsar <pulsarmn@yandex.ru>
2026-09-16 11:21:43 +0300

    test: add tests on the UserMapper class

---
+++ src/test/java/ru/pulsarmn/messenger/user/mapper/UserMapperTest.java
@@ -0,0 +1,32 @@
+package ru.pulsarmn.messenger.user.mapper;
+
+import org.junit.jupiter.api.Test;
+import ru.pulsarmn.messenger.user.domain.User;
+import ru.pulsarmn.messenger.user.dto.UserSearchResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class UserMapperTest {
+
+    private final UserMapper userMapper = new UserMapper();
+
+    @Test
+    void mapToSearchResponse_whenCorrectUser_shouldMapUserToResponse() {
+        User user = User.builder()
+                .username("correct-username")
+                .build();
+
+        UserSearchResponse actualResult = userMapper.mapToSearchResponse(user);
+
+        assertThat(actualResult).isNotNull();
+        assertThat(actualResult.username()).isEqualTo(user.getUsername());
+    }
+
+    @Test
+    void mapToSearchResponse_whenNullUser_shouldThrowNullPointerException() {
+        assertThatThrownBy(() -> userMapper.mapToSearchResponse(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+}
