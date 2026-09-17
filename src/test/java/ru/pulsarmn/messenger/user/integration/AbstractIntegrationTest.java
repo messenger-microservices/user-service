da97910d6f3f5908363ce81f678e36b10018c1f2
pulsar <pulsarmn@yandex.ru>
2026-09-17 09:17:31 +0300

    feat: add the @IntegrationTest annotation

--- src/test/java/ru/pulsarmn/messenger/user/integration/AbstractIntegrationTest.java
+++ src/test/java/ru/pulsarmn/messenger/user/integration/AbstractIntegrationTest.java
@@ -1,15 +1,13 @@
 package ru.pulsarmn.messenger.user.integration;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+import ru.pulsarmn.messenger.user.integration.annotation.IntegrationTest;
 
 
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@IntegrationTest
 public class AbstractIntegrationTest {
 
     private static final String DEFAULT_USERNAME = "test";
