77cbf64f383b7686c29f0cf28f67296e45aba796
pulsar <pulsarmn@yandex.ru>
2026-09-18 10:13:43 +0300

    feat: add additional exception handling for commont exceptions

--- src/main/java/ru/pulsarmn/messenger/user/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/pulsarmn/messenger/user/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.pulsarmn.messenger.user.exception.InvalidHeaderException;
@@ -32,5 +33,23 @@ public class GlobalExceptionHandler {
     ProblemDetail handleInvalidHeader(InvalidHeaderException ex) {
         log.warn("Invalid user id header", ex);
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Invalid user id");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    ProblemDetail handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.warn("Invalid input data", ex);
+        return ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ProblemDetail handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        log.warn("Invalid method argument(-s)", ex);
+        return ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    ProblemDetail handleException(Exception ex) {
+        log.error("An unexpected error occurred", ex);
+        return ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
