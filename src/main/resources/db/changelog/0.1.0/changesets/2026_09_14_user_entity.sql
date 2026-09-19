1da1fec22e834a37a3b462890b03950466379434
pulsar <pulsarmn@yandex.ru>
2026-09-19 11:28:16 +0300

    fix: remove the String passwordHash from the User entity

--- src/main/resources/db/changelog/0.1.0/changesets/2026_09_14_user_entity.sql
+++ src/main/resources/db/changelog/0.1.0/changesets/2026_09_14_user_entity.sql
@@ -5,10 +5,11 @@ CREATE TABLE users
 (
     id            UUID PRIMARY KEY,
     username      VARCHAR(32)  NOT NULL UNIQUE,
-    password_hash VARCHAR(256) NOT NULL,
     phone_number  VARCHAR(50)  NULL,
     display_name  VARCHAR(64)  NOT NULL,
     birthdate     DATE         NULL,
     created_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     updated_at    TIMESTAMPTZ  NOT NULL DEFAULT NOW()
 );
+
+--rollback DROP TABLE users;
